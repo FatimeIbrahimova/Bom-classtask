97888190b327931846dea83d9dfbee8766becc2a
FatimeIbrahimova <fatime.ibrahimova44@gmail.com>
2022-12-01 13:27:12 +0400

    classtask

---
+++ script.js
@@ -0,0 +1,32 @@
+// document.getElementById("demo").innerHTML =
+// "Screen Height: " + window.screen.width;
+
+// document.getElementById("Demo").innerHTML =
+// "Available Screen Width: " + window.screen.availWidth;
+// let title= document.getElementById("demo");
+// window.addEventListener("resize",() =>{
+//    title.innerHTML ="Screen Height: " + window.screen.width;
+// })
+// document.getElementById("demo").innerHTML =window.location.href;
+
+// document.getElementById("demo").innerHTML =window.location.hostname;
+
+// document.getElementById("demo").innerHTML =window.location.protocol;
+
+// let button=document.querySelector("button");
+// button.addEventListener("click",() =>{
+//    window.location.assign("https://www.w3schools.com")
+// })
+// let button=document.querySelector("button");
+// button.addEventListener("click",() =>{
+//     window.location.reload();
+// }
+
+let button=document.querySelector("button");
+button.addEventListener("click",() =>{
+    window.open()
+})
+button.addEventListener("click",() =>{
+    window.close()
+})
+
